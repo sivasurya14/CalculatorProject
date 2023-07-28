14d1d3ed8b95f136066f8b00c0f0c56fc350d222
sivasuriyan <sivasuriyan0120@gmail.com>
2023-07-28 16:19:08 +0530

    First commit

---
+++ src/calculation/Calculator.java
@@ -0,0 +1,18 @@
+package calculation;
+
+public class Calculator {
+
+	public static int add(int a, int b) {
+
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+
+		
+		
+		System.out.println("addition of two number is : "+Calculator.add(10, 10));
+
+	}
+
+}
